175ce9523b443cd3669d574078b8ea163f495ae7
AaronM <aaron.massingham@outlook.com>
2023-08-26 17:51:28 +0100

    Test for build error

--- src/components/buttons/Default.tsx
+++ src/components/buttons/Default.tsx
@@ -1,7 +1,7 @@
 import React, { forwardRef } from "react";
 
 //Types
-import { DefaultButtonType } from "@typescriptTypes/ComponentTypes";
+// import { DefaultButtonType } from "@typescriptTypes/ComponentTypes";
 
 //Styled Components
 import { Container } from "@styles/styled-components/buttons/DefaultAlt";
@@ -17,5 +17,13 @@ const DefaultButton = forwardRef<HTMLButtonElement, DefaultButtonType>(
 );
 
 DefaultButton.displayName = "Button";
+
+type DefaultButtonType = {
+	text: string | React.ReactNode;
+	isLoading?: boolean;
+	buttonType?: "submit" | "reset" | "button";
+	fullw?: boolean;
+	onClick?: React.MouseEventHandler<HTMLButtonElement>;
+};
 
 export default DefaultButton;
