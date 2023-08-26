b43fef5710c1ff6607052905d789f173a3fbe6d3
AaronM <aaron.massingham@outlook.com>
2023-08-26 17:41:56 +0100

    Update title case for file names

--- src/components/forms/SearchExercise.tsx
+++ src/components/forms/SearchExercise.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { motion } from "framer-motion";
 
 //Types
-import { SearchExerciseType } from "@typescriptTypes/searchTypes";
+import { SearchExerciseType } from "@typescriptTypes/SearchTypes";
 
 //Helpers
 import { filterNamesBySearchTerm } from "@helpers/filterHelpers";
